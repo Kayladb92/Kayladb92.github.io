82af3657c7bb570b2a2b379c6c58658e0d0e5489
Kayladb92 <kaylab92@gmail.com>
2023-03-01 18:59:46 -0500

    j

---
+++ js_calc_template/js/calculator.js
@@ -0,0 +1,11 @@
+let result = document.getElementById("result");
+    
+function displayTwoPlusTwo()  {
+    let number1 =2;
+    let number2 = 2;
+    result.innerHTML = number1 + number2;
+}
+
+function displayThreeTimesFour() {
+    result.innerHTML = 3 * 4;
+}
