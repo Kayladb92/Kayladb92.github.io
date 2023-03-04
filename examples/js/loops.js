0041825b2e1bdd58cfd53e7aac9b3ca6f790eda5
Kayladb92 <kaylab92@gmail.com>
2023-03-03 19:29:54 -0600

    Update loops.js

--- examples/js/loops.js
+++ examples/js/loops.js
@@ -1,5 +1,9 @@
-const body = document.getElementsByTagName("body")[0];
-body.innerHTML += "<h2>Heading 1</h2>";
-body.innerHTML += "<h2>Heading 2</h2>";
-body.innerHTML += "<h2>Heading 3</h2>";
-body.innerHTML += "<h2>Heading 4</h2>";
+// const body = document.getElementsByTagName("body")[0];
+// body.innerHTML += "<h2>Heading 1</h2>";
+// body.innerHTML += "<h2>Heading 2</h2>";
+// body.innerHTML += "<h2>Heading 3</h2>";
+// body.innerHTML += "<h2>Heading 4</h2>";
+
+for (let i = 0; i < 10; i++) {
+    body.innerHTML += "<h2>Heading " + (i+1) + "</h2>";
+}
